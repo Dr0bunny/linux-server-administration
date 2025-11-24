defa1ec052ca565c8f6b2490deb04472dec10402
VxRabbit <cx780496@gmail.com>
2025-11-24 01:58:53 +0100

    Remove main execution block in app.py
    Remove the main execution block for the Flask app.

--- app.py
+++ app.py
@@ -5,6 +5,3 @@ app = Flask(__name__, template_folder='/var/www/company.net/html')
 @app.route('/')
 def index():
     return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
